a8cb23060de1f325609e618513af5040f7a507c1
wwqgtxx <wwqgtxx@gmail.com>
2023-03-16 20:36:46 +0800

    expose MaxDatagramFrameSize config

--- config_test.go
+++ config_test.go
@@ -126,6 +126,8 @@ func configWithNonZeroNonFunctionFields(t *testing.T) *Config {
 			f.Set(reflect.ValueOf(true))
 		case "Allow0RTT":
 			f.Set(reflect.ValueOf(true))
+		case "MaxDatagramFrameSize":
+			f.Set(reflect.ValueOf(int64(0)))
 		default:
 			t.Fatalf("all fields must be accounted for, but saw unknown field %q", fn)
 		}

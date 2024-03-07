185b80e59a63aca0a67e411d774c33f2e7cecdd8
wwqgtxx <wwqgtxx@gmail.com>
2024-03-08 00:41:42 +0800

    expose SetRemoteAddr to Connection

--- connection.go
+++ connection.go
@@ -2689,3 +2689,7 @@ func estimateMaxPayloadSize(mtu protocol.ByteCount) protocol.ByteCount {
 func (s *connection) SetCongestionControl(cc congestion.CongestionControl) {
 	s.sentPacketHandler.SetCongestionControl(cc)
 }
+
+func (s *connection) SetRemoteAddr(addr net.Addr) {
+	s.conn.SetRemoteAddr(addr)
+}
